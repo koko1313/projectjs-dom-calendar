8395a3ba2b72aba60c8b2a2d2477aa40e61bbe49
koko1313 <zakakvotie@abv.bg>
2019-02-02 01:00:52 +0200

    Added method chaining

--- myDomLib.js
+++ myDomLib.js
@@ -1,190 +1,142 @@
 var myDomLib = {
-
-    /** 
-     * Взима елемент по id или class
-     * @param {string} element id (#) или class (.)
-     * @returns {object} връща намерения обект
-    */
     get: function(element) {
-        var elementType = element[0]; // взимаме типа на елемента (#/.)
-        elementIdentificator = element.substring(1, element.length);
+        var elementObject = {
 
-        switch(elementType) {
-            case "#":
-                return document.getElementById(elementIdentificator);
-            case ".":
-                return document.getElementsByClassName(elementIdentificator);
-        }
-    },
+            element: null, // ще съхранява самия елемент
 
-    /**
-     * Добавя елемент към елемент
-     * @param {string} id id на елемента, към който ще се добави
-     * @param {string} element елемента, който ще се създаде
-     * @param {string} elementId id на елемента, който ще се създаде
-     */
-    append: function(id, element, elementId) {
-        var parentElement = this.get("#"+id);
-        
-        var newElement = document.createElement(element);
-        
-        if(elementId != undefined) {
-            newElement.setAttribute("id", elementId);
-        }
+            // set-ва елемента
+            set: function(element) {
+                elementObject.element = document.querySelector(element);
+            },
 
-        parentElement.appendChild(newElement);
-    },
+            // и тук има функция get, за да може да се прави вложен get
+            get: function(element) {
+                // проверяваме дали търсения елемент се намира вътре в текущия
+                if(this.element.querySelector(element)) {
+                    return myDomLib.get(element);
+                }
+            },
 
-    /**
-     * Добавя текст в елемент
-     * @param {string} id id на елемента, към който ще бъде добавен текста
-     * @param {string} text текста, който ще се добави
-     * @param {boolean} replace да бъде ли заместен съществуващия текст
-     */
-    appendText: function(id, text, replace) {
-        var parentElement = this.get("#"+id);
+            /**
+             * Добавя елемент
+             * @param {string} element елемента, който ще се създаде
+             * @param {string} elementId id на елемента, който ще се създаде
+             */
+            append: function(element, elementId) {
+                var parentElement = this.element;
+                
+                var newElement = document.createElement(element);
+                
+                if(elementId != undefined) {
+                    newElement.setAttribute("id", elementId);
+                }
 
-        if(replace) {
-            // изтриваме всички child елементи
-            while (parentElement.firstChild) {
-                parentElement.removeChild(parentElement.firstChild);
+                parentElement.appendChild(newElement);
+            },
+
+            /**
+             * Добавя текст
+             * @param {string} text текста, който ще се добави
+             * @param {boolean} replace да бъде ли заместен съществуващия текст
+             */
+            appendText: function(text, replace) {
+                var parentElement = this.element;
+
+                if(replace) {
+                    // изтриваме всички child елементи
+                    while (parentElement.firstChild) {
+                        parentElement.removeChild(parentElement.firstChild);
+                    }
+                }
+
+                var textNode = document.createTextNode(text);
+                parentElement.appendChild(textNode);
+            },
+
+            /**
+             * Връща текста, който се съдържа в елемента
+             * @returns {string} текста, който се съдържа в елемента
+             */
+            getText: function() {
+                return this.element.innerText;
+            },
+
+            /**
+             * Добавя HTML
+             * @param {string} html html-а, който ще се добави
+             * @param {boolean} replace да бъде ли заместен съществуващия HTML
+             */
+            appendHTML: function(html, replace) {
+                var parentElement = this.element;
+
+                if(replace) {
+                    parentElement.innerHTML = html;
+                } else {
+                    parentElement.innerHTML += html;
+                }
+            },
+
+            /**
+             * Връща HTML-а, който се съдържа в елемента
+             * @returns {string} HTML-a, който се съдържа в елемента
+             */
+            getHTML: function() {
+                return this.element.innerHTML;
+            },
+
+            /**
+             * Добавя атрибут
+             * @param {string} attr атрибута, който ще бъде добавен
+             * @param {string} attrValue стойността на атрибута
+             */
+            appendAttr: function(attr, attrValue) {
+                this.element.setAttribute(attr, attrValue);
+            },
+
+            /**
+             * Добавя стил към елемент
+             * @param {object} styleCollection масив с колекции от вида {property: "color", value: "red"}
+             */
+            setStyle: function(styleCollection) {
+                for(var i=0; i<styleCollection.length; i++) {
+                    this.element.style.setProperty(styleCollection[i].property, styleCollection[i].value);
+                }
+            },
+
+            /**
+             * Връща родителския елемент
+             * @returns {object} родителския елемент
+             */
+            getParent: function() {
+                return this.element.parentElement;
+            },
+
+            /**
+             * Връща следващия съседен елемент
+             * @returns {object} следващия елемент
+             */
+            getNextSibling: function() {
+                return this.element.nextElementSibling;
+            },
+
+            /**
+             * Връща предишния съседен елемент
+             * @returns {object} предишния елемент
+             */
+            getPreviousSibling: function() {
+                return this.element.previousElementSibling;
+            },
+
+            /**
+             * Връща децата на елемент
+             * @returns {Array} масив с елементите, които са деца
+             */
+            getChildrens: function() {
+                return this.element.children;
             }
         }
 
-        var textNode = document.createTextNode(text);
-        parentElement.appendChild(textNode);
-    },
-
-    /**
-     * Връща текста, който се съдържа в елемента
-     * @param {string} id id на елемента, на който ще вземем текста
-     * @returns {string} текста, който се съдържа в елемента
-     */
-    getText: function(id) {
-        var element = this.get("#"+id);
-        return element.innerText;
-    },
-
-    /**
-     * Добавя HTML към елемент
-     * @param {string} id id на елемента, към който ще бъде добавен html-а
-     * @param {string} html html-а, който ще се добави
-     * @param {boolean} replace да бъде ли заместен съществуващия HTML
-     */
-    appendHTML: function(id, html, replace) {
-        var parentElement = this.get("#"+id);
-
-        if(replace) {
-            parentElement.innerHTML = html;
-        } else {
-            parentElement.innerHTML += html;
-        }
-
-    },
-
-    /**
-     * Връща HTML-а, който се съдържа в елемента
-     * @param {string} id id на елемента, на който ще вземем HTML-а
-     * @returns {string} HTML-a, който се съдържа в елемента
-     */
-    getHTML: function(id) {
-        var element = this.get("#"+id);
-        return element.innerHTML;
-    },
-
-    /**
-     * Добавя атрибут към елемент
-     * @param {string} id id на елемента, към който ще бъде добавен атрибут
-     * @param {string} attr атрибута, който ще бъде добавен
-     * @param {string} attrValue стойността на атрибута
-     */
-    appendAttr: function(id, attr, attrValue) {
-        var element = this.get("#"+id);
-        element.setAttribute(attr, attrValue);
-    },
-
-    /**
-     * Добавя стил към елемент
-     * @param {string} id id на елемента, към който ще бъде добавен стила
-     * @param {object} styleCollection масив с колекции от вида {property: "color", value: "red"}
-     */
-    setStyle: function(id, styleCollection){
-        var element = this.get("#"+id);
-
-        for(var i=0; i<styleCollection.length; i++) {
-            element.style.setProperty(styleCollection[i].property, styleCollection[i].value);
-        }
-    },
-
-    /**
-     * Връща родителския елемент
-     * @param {*} id id на елемента, на който ще върне родителския елемент
-     * @returns {object} родителския елемент
-     */
-    getParent: function(id) {
-        var element = this.get("#"+id);
-        return element.parentElement;
-    },
-
-    /**
-     * Връща следващия съседен елемент
-     * @param {string} id id на елемента, на който ще върне следващия
-     * @returns {object} следващия елемент
-     */
-    getNextSibling: function(id) {
-        var element = this.get("#"+id);
-        return element.nextElementSibling;
-    },
-
-    /**
-     * Връща предишния съседен елемент
-     * @param {string} id id на елемента, на който ще върне предишния
-     * @returns {object} предишния елемент
-     */
-    getPreviousSibling: function(id) {
-        var element = this.get("#"+id);
-        return element.previousElementSibling;
-    },
-
-    /**
-     * Връща децата на елемент
-     * @param {string} id id на елемента, на който ще върне децата
-     * @returns {Array} масив с елементите, които са деца
-     */
-    getChildrens: function(id) {
-        var element = this.get("#"+id);
-        return element.children;
+        elementObject.set(element);
+        return elementObject;
     }
 };
-
-myDomLib.appendText("div1", "sad---");
-myDomLib.appendText("div1", "dd", true); // презаписва съществуващия текст
-
-myDomLib.getText("div1");
-
-
-myDomLib.append("div1", "div", "myDiv");
-myDomLib.appendAttr("myDiv", "class", "myClass");
-myDomLib.appendAttr("myDiv", "class", "myClass2"); // презаписваме атрибута
-
-myDomLib.appendHTML("div2", "<h1>asd</h1>");
-myDomLib.appendHTML("div2", "<h3>asd3</h3>", true); // презаписва HTML-а
-
-myDomLib.getHTML("div1");
-
-myDomLib.setStyle("div1", [
-    {property: "border", value: "10px solid black"},
-    {property: "background", value: "red"},
-]);
-
-// презаписваме стила на рамката
-myDomLib.setStyle("div1", [
-    {property: "border", value: "1px solid black"},
-]);
-
-myDomLib.getParent("myDiv");
-
-myDomLib.getNextSibling("list1");
-myDomLib.getPreviousSibling("list2");
-
-myDomLib.getChildrens("div3");
